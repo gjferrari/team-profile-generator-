916c9a7bca20a939a7c61a7793ab70b6a05531e7
Genevieve Ferrari <genferrari@gmail.com>
2022-12-08 01:03:35 -0800

    added error tests

--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -1,30 +1,27 @@
 const Engineer = require("../lib/Engineer");
 
 describe("Engineer", () => {
-  it("should create an object with a name, id number, email, and github if provided valid arguments", () => {
+  it("should create an employee object with github added to the class using the getGitHub method", () => {
+    const testHub = "ethan40";
+    const engineer = new Engineer(
+      "Ethan",
+      20,
+      "enniemeanie@gmail.com",
+      testHub
+    );
+    expect(engineer.getGitHub()).toBe(testHub);
+  });
+
+  //testing getRole
+  it("should return role as Engineer", () => {
+    const testRole = "Engineer";
     const engineer = new Engineer(
       "Ethan",
       20,
       "enniemeanie@gmail.com",
       "ethan40"
     );
-
-    expect(employee.getName).toEqual("Ethan");
-    expect(employee.getID).toEqual(20);
-    expect(employee.getEmail).toEqual("enniemeanie@gmail.com");
-    expect(employee.getGitHub).toEqual("ethan40");
-  });
-
-  //gitName
-  it("should throw an error if provided no argument", () => {
-    //error is thrown when callback is empty
-    expect(() => new Employee()).toThrow();
-  });
-
-  it("should throw an error if provided no argument", () => {
-    //error is thrown when callback is empty
-    expect(() => new Employee()).toThrow();
-    
+    expect(engineer.getRole()).toBe(testRole);
   });
 
   it("should throw an error if provided no argument", () => {
@@ -32,3 +29,4 @@ describe("Engineer", () => {
     expect(() => new Employee()).toThrow();
   });
 });
+
