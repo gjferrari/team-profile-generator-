1273cfc378d9e62cc3f710245ce99a4e338d3ca9
Genevieve Ferrari <genferrari@gmail.com>
2022-12-08 00:24:10 -0800

    cleaned up test

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -16,26 +16,4 @@ describe("Employee", () => {
     //error is thrown when callback is empty
     expect(() => new Employee()).toThrow();
   });
-
-  it("should get name through getName method", () => {
-    // const testName = "Sam";
-    const employee = new Employee("Sam", 69, "sammyboy@gmail.com");
-    expect(employee.getName()).toBe("Sam");
-    expect(employee.getID()).toBe(69);
-    expect(employee.getEmail()).toBe("sammyboy@gmail.com");
-  });
-
-  // it("should get id through getID method", () => {
-  //   const testID = 69;
-  //   const employee = new Employee(testID);
-  //   expect(employee.getID()).toBe(testID);
-  // });
-
-  // it("should get email through getEmail method", () => {
-  //   const testE = "sammyboy@gmail.com";
-  //   const employee = new Employee(testE);
-  //   expect(employee.getEmail()).toBe(testE);
-  // });
 });
-
-//unfinished
