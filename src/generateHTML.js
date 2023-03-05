86d6a8d58aeb0902fcaae5955463ffa153517e44
Genevieve Ferrari <genferrari@gmail.com>
2023-03-04 17:43:35 -0800

    added html for all roles

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -2,25 +2,65 @@
 
 const createManager = (manager) => {
   //return is implied in arrow functions
-  `<div class="card employee-info">
-    <div class="card-header bg-primary text-white>
-        <h2 class="card-title>${manager.name}</h2>
+  `<div class="col-4 mt-4">
+  <div class="card h-100">
+    <div class="card-header bg-primary text-white">
+      <h3>${manager.name}</h3>
+      <h4>Manager</h4>
+      <i class="fa-solid fa-person-harassing"></i>
     </div>
-  </div>`
-    
-        ;
-};
-
-const createIntern = (intern) => {
-  //return is implied in arrow functions
-  `<div class="card employee-info">
-    <div class="card-header bg-primary text-white
-        <h2 class="card-title>${intern.name}</h2>`;
+    <div class="card-body">
+      <p class="id">ID: ${manager.id}</p>
+      <p class="email">
+        Email: <a href="mailto:${manager.email}">${manager.email}</a>
+      </p>
+      <p class="office">Office Number: ${manager.officeNumber}</p>
+    </div>
+  </div>
+</div>`;
 };
 
 const createEngineer = (engineer) => {
   //return is implied in arrow functions
-  `<div class="card employee-info">
-    <div class="card-header bg-primary text-white
-        <h2 class="card-title>${engineer.name}</h2>`;
+  `<div class="col-4 mt-4">
+  <div class="card h-100">
+    <div class="card-header bg-primary text-white">
+      <h3>${engineer.name}</h3>
+      <h4>Engineer</h4>
+      <i class="fa-solid fa-person-harassing"></i>
+    </div>
+    <div class="card-body">
+      <p class="id">ID: ${engineer.id}</p>
+      <p class="email">
+        Email: <a href="mailto:${engineer.email}">${engineer.email}</a>
+      </p>
+      <p class="github">Github: ${engineer.gitHub}</p>
+    </div>
+  </div>
+</div>`;
 };
+
+const createIntern = (intern) => {
+  //return is implied in arrow functions
+  `<div class="col-4 mt-4">
+  <div class="card h-100">
+    <div class="card-header bg-primary text-white">
+      <h3>${intern.name}</h3>
+      <h4>Intern</h4>
+      <i class="fa-solid fa-person-harassing"></i>
+    </div>
+    <div class="card-body">
+      <p class="id">ID: ${intern.id}</p>
+      <p class="email">
+        Email: <a href="mailto:${intern.email}">${intern.email}</a>
+      </p>
+      <p class="school">School: ${intern.school}</p>
+    </div>
+  </div>
+</div>`;
+};
+
+const createPage = () => {
+  if Employee.role === Manager 
+   
+}
